57ad7fe2fffa4f4d667611483760a4ae3983f784
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-07-11 12:40:01 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,6 +9,16 @@ void find_the_sum(int *num1,int *num2,int *num3,int **sum)
   **sum = *num1 + *num2 + *num3;
 }
 
+void find_num_greater_than_100(int *num1,int *num2,int *num3)
+{
+ if(*num1 > 1000)
+  *num1 = 0;
+ if(*num2 > 1000)
+  *num2 = 0;
+ if(*num3 > 1000)
+  *num3 = 0;
+}
+
 void process_input(const char *input, int *sum)
 {
   int num1 = 0;
@@ -23,6 +33,8 @@ void process_input(const char *input, int *sum)
   {
     sscanf(input, "%d\n%d,%d", &num1,&num2,&num3);
   }
+
+  find_num_greater_than_100(&num1,&num2,&num3);
   find_the_sum(&num1,&num2,&num3,&sum);
   
 }
