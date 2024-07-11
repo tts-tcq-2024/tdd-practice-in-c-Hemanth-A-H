8d0599fef24859a37fd8540e14ebcd03ce06dd84
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-07-11 09:25:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,15 +1,28 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-int add(const char *input)
+#include <stdio.h>
+#include <stdlib.h>
+
+int check_input_zero(int num1, int num2)
 {
-  return 0;
+  if((num1 == 0) && (num2 == 0))
+  {
+    return 0;
+  }
+  return 1;  
 }
 
-
-
-
-
-
+int add(const char *input)
+{
+  int sum = 0;
+  int num1,num2;
+  sscanf(input, "%d,%d", &num1,&num2);
+  if(check_input_zero(num1,num2))
+  {
+    sum = num1+num2;
+  } 
+  return sum;
+}
 
 #endif 
