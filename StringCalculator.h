7ffe96431b75988ffa2413f6b7ac8c951e5462d2
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-07-11 16:29:21 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,22 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int delimiter_flag = 0;
+int temp = 0;
+
+void checkforgreater_than1000(int *temp)
+{
+  if(*temp > 1000)
+  {
+    *temp = 0;
+  }  
+}
+
+void checkfordigits(char num_char,int *sum)
+{
+  if(isdigit(num_char) && (delimiter_flag == 0))
+  {
+    temp = temp * 10 + (num_char - '0');
+    checkforgreater_than1000(&temp);    
+  }
+  else
+  {
+    delimiter_flag = 1;
+    *sum = *sum + temp;
+    temp = 0;      
+  }
+}
+
 
 void process_input(const char *input, int *sum)
 {
-  int num1 = 0;
-  int num2 = 0;
-  int num3 = 0;
   int i = 0;
   int input_len = strlen(input);
   while(i<input_len)
   {
-      if(isdigit(input[i]))
-      {
-          *sum = *sum + (input[i] - '0');
-      }
+    checkfordigits(input[i],sum);
     i++;
-  }  
+    if(delimiter_flag == 1)
+    {
+        delimiter_flag = 0;
+    }
+  }
+  *sum = *sum + temp;  
 }
 
 
